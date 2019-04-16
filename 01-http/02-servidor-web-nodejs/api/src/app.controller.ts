ebf8c9308a4342b5277f36132115e4cd17d011cd
JennyCastro <jennykarinacastro@gmail.com>
2019-04-16 16:00:35 -0500

    implementar cabeceras

--- 01-http/02-servidor-web-nodejs/api/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api/src/app.controller.ts
@@ -1,20 +1,89 @@
-import {Controller, Get, HttpCode, Post} from '@nestjs/common';
+import {Controller, Delete, Get, Headers, HttpCode, Post, Put} from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller()
+import {puts} from "util";
+import {get} from "https";
+import {isUndefined} from "../node_modules/@nestjs/common/utils/shared.utils";
+//protocolo-ip-puerto
+//http://192.168.1.10:3000/segmentoInicial
+//http://192.168.1.10:3000/mascota/crear
+//http://192.168.1.10:3000/mascota/borrar
+//@Controller(segmentoInicial)
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get() //Metodo HTTP
-  getHello(): string {
-    return this.appService.getHello();
-  }
-@Post() // Metodo http
 
-@HttpCode(200)
-  postHello(){
-    return 'Hola mundo en post';
-}
+//@Controller(segmentoAccion)
+    @Get('/hello-world') // Metodo http
+
+    helloWorld(){
+        return 'hello-world';
+    }
+
+@Post('/hola-mundo') // Metodo http
+
+  holaMundo(){
+    return 'Hola-mundo';
+  }
+
+    @Delete('/salut-monde') // Metodo http
+
+    deleteHello(){
+        return 'salut-monde';
+    }
+
+    @Put('/ola-mundo')
+    olaMundo(){
+        return 'ola-mundo';
+    }
+
+    @Get('/adivina')
+
+    adivina(@Headers() headers): string {
+    console.log('Headers:', headers);
+        const numeroRandomico = Math.round(Math.random()*10);
+        const numeroCabecera = Number(headers.numero);
+        if (numeroCabecera == numeroRandomico){
+          return 'ok'
+        }else{
+          return ':('
+        }
+
+          return 'ok';
+    }
+
+    /*
+    //datos primitivos
+  //js -ts
+  */
+
+
+    /*
+var nombre= 'JENNY'; //string
+  var edad = 29; //nummber
+  var sueldo = 1.20; //number
+  var casado = false; //boolean
+  var hijos = null; //null
+  var alas = undefined; //undefied
+
+//let
+let nombre1= 'JENNY'; //string
+let edad1 = 29; //nummber
+let sueldo1 = 1.20; //number
+let casado1 = false; //boolean
+let hijos1 = null; //null
+let alas1 = undefined;
+
+  /*
+  Segmento incial: /api
+  1 Segmento accion : GET ''hello-world' ->'Hello world'
+  2 Segmento accion : POST 'hola-mundo'
+  3 Segmento accion : PUT '-----'
+  4 Segmento accion : DELETE '-----'
+     */
+
+
+
 }
 /*
 @nombreDecorador
@@ -38,3 +107,26 @@ class usuario {
 
 }
 */
+
+const json = [
+    {"llave":"valor"}
+
+]
+;
+
+let objeto:any = {
+  propiedad: 'valor',
+    propiedadDOs: 'valor2'
+};
+
+objeto.propiedad //valor
+objeto.propiedadDOs //valor
+
+//crear propiedades a un objeto
+
+objeto.propiedadTres = 'valor3';
+objeto['propiedadTres'] = 'VALOR 3';
+
+//Eliminar propiedad
+delete objeto.propiedadTres; //--> //destruir
+objeto.propiedadTres = undefined;
