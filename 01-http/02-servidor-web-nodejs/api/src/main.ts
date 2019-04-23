d5870c1af6f0a233238a466448688f812155216d
JennyCastro <jennykarinacastro@gmail.com>
2019-04-23 15:53:45 -0500

    cookies seguras

--- 01-http/02-servidor-web-nodejs/api/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api/src/main.ts
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  app.use(cookieParser('Me gusta el trago'));
   await app.listen(3000);
 }
 bootstrap();
