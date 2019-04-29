b43408e88142cf6c99dff477e4d4da00ae3bf229
JennyCastro <jennykarinacastro@gmail.com>
2019-04-29 01:12:54 -0500

    Cookies seguras_deber

--- 02-deber/calculadora/api-calculadora/src/app.controller.ts
+++ 02-deber/calculadora/api-calculadora/src/app.controller.ts
@@ -94,7 +94,7 @@ export class AppController {
             const numeroDos = Number(body.numero2);
 
             const resultado = numeroUno - numeroDos;
-            
+
             const cookieSeguras =request.signedCookies.nombre;
 
             if(cookieSeguras){
@@ -130,17 +130,59 @@ export class AppController {
 
     }
 
-    @Put('/multiplicacion')
+    @Get('/multiplicacion')
     @HttpCode(202)
-    multiplicar(@Query() query, @Response() mult) {
+    multiplicar(@Query() query, @Response() response, @Request() request) {
+
+        const cookieSegura = request.signedCookies;
+        const cookies = request.cookies;
+
+        const esquemaValidacion= Joi.object().keys({
+            nombre : Joi.string().required()
+        });
+
+        const objetoValidacion ={
+            nombre: cookies.userName
+        };
+        const respuesta = Joi.validate(objetoValidacion, esquemaValidacion);
+
         if(query.numero1 && query.numero2){
+
             const numeroUno = Number(query.numero1);
             const numeroDos = Number(query.numero2);
-            //console.log(query);
-            const respuesta = numeroUno * numeroDos;
-            return mult.status(202).send({'El valor de la multiplicación es: ': `${respuesta}`});
-        }else{
-            return mult.status(400).send({mensaje: 'Error, parámetros erróneos', error: 400});
+
+            const resultado = numeroUno * numeroDos;
+
+            const cookieSeguras =request.signedCookies.nombre;
+
+            if(cookieSeguras){
+                console.log('Cookie segura', cookieSeguras);
+            }else{
+                console.log('No es valida esta cookie');
+            }
+
+            if(!cookieSegura.userName){
+                response.cookie('nombre', 'Jenny', {signed:true});
+            }
+            if(!cookieSegura.valor){
+                response.cookie('valor', 100, {signed:true})
+            }
+
+            const intento= cookieSegura.valor - resultado;
+            if(intento<=0){
+                response.cookie('valor', 100, {signed:true})
+                response.status(203).send({NombreUsuario: `${cookieSeguras}`,
+                    Resultado: `${resultado}`,
+                    Mensaje: "Se terminaron sus puntos"})
+            }else{
+                response.cookie('valor', intento, {signed:true})
+                response.status(203).send({
+                    'NombreUsuario: ': `${cookieSeguras}`, 'Resultado: ': `${resultado}`})
+                //console.log(cookieSeguras);
+            }
+
+        }else {
+            response.status(400).send({mensaje: 'Error, Parámetros inválidos', error: 400});
         }
 
 
@@ -148,20 +190,64 @@ export class AppController {
 
     @Delete('/division')
     @HttpCode(203)
-    dividir(@Body() body, @Headers() headers, @Response() div) {
+    dividir(@Body() body, @Headers() headers, @Response() response, @Request() request) {
+
+        const cookieSegura = request.signedCookies;
+        const cookies = request.cookies;
+
+        const esquemaValidacion= Joi.object().keys({
+            nombre : Joi.string().required()
+        });
+
+        const objetoValidacion ={
+            nombre: cookies.userName
+        };
+        const respuesta = Joi.validate(objetoValidacion, esquemaValidacion);
+
         if(body.numero1 && headers.numero2){
+
             const numeroUno = Number(body.numero1);
             const numeroDos = Number(headers.numero2);
-            //console.log(body, headers);
+
             if(numeroDos !=0){
-                const respuesta = numeroUno / numeroDos;
-                div.status(203).send({'El valor de la división es: ': `${respuesta}`});
+                const resultado = numeroUno / numeroDos;
+                const cookieSeguras =request.signedCookies.nombre;
+
+                if(cookieSeguras){
+                    console.log('Cookie segura', cookieSeguras);
+                }else{
+                    console.log('No es valida esta cookie');
+                }
+
+                if(!cookieSegura.userName){
+                    response.cookie('nombre', 'Jenny', {signed:true});
+                }
+                if(!cookieSegura.valor){
+                    response.cookie('valor', 100, {signed:true})
+                }
+
+                const intento= cookieSegura.valor - resultado;
+                if(intento<=0){
+                    response.cookie('valor', 100, {signed:true})
+                    response.status(203).send({NombreUsuario: `${cookieSeguras}`,
+                        Resultado: `${resultado}`,
+                        Mensaje: "Se terminaron sus puntos"})
+                }else{
+                    response.cookie('valor', intento, {signed:true})
+                    response.status(203).send({
+                        'NombreUsuario: ': `${cookieSeguras}`, 'Resultado: ': `${resultado}`})
+                    //console.log(cookieSeguras);
+                }
+
+
             }else{
-                div.send({mensaje:'No existe división para CERO'});
+                response.send({Mensaje:'No existe división para CERO'});
             }
-        }else{
-            return div.status(400).send({mensaje: 'Error, parámetros erróneos', error: 400});
+
+        }else {
+            response.status(400).send({mensaje: 'Error, Parámetros inválidos', error: 400});
         }
+        
     }
 
 
