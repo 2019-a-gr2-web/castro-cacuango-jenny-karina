ecfcb2cb769e1cafa5413c7d4847b291656a7a90
JennyCastro <jennykarinacastro@gmail.com>
2019-04-30 14:15:09 -0500

    Calculadora

--- 02-deber/calculadora/api-calculadora/src/app.controller.ts
+++ 02-deber/calculadora/api-calculadora/src/app.controller.ts
@@ -1,6 +1,7 @@
 import {Controller, Get, Headers, Response, Post, Body, HttpCode, Put, Query, Delete, Header, Request} from '@nestjs/common';
 import { AppService } from './app.service';
 import * as Joi from '@hapi/joi';
+import {async} from "rxjs/internal/scheduler/async";
 
 @Controller('/calculadora')
 export class AppController {
@@ -15,19 +16,9 @@ export class AppController {
     @HttpCode(200)
     sumar(@Headers() headers, @Response() response, @Request() request) {
 
-        const cookieSegura = request.signedCookies;
-        const cookies = request.cookies;
 
-        const esquemaValidacion= Joi.object().keys({
-            nombre : Joi.string().required()
-        });
 
-        const objetoValidacion ={
-            nombre: cookies.userName
-        };
-        const respuesta = Joi.validate(objetoValidacion, esquemaValidacion);
-
-        if(headers.numero1 && headers.numero2){
+        if(headers.numero1 && headers.numero2) {
 
             const numeroUno = Number(headers.numero1);
             const numeroDos = Number(headers.numero2);
@@ -35,35 +26,51 @@ export class AppController {
             const resultado = numeroUno + numeroDos;
 
 
-            const cookieSeguras =request.signedCookies.nombre;
+            const cookieSeguras = request.signedCookies;
+            console.log(cookieSeguras.nombre);
+            console.log(cookieSeguras.valor);
 
-            if(cookieSeguras){
-                console.log('Cookie segura', cookieSeguras);
-            }else{
-                console.log('No es valida esta cookie');
+            console.log(cookieSeguras);
+            console.log(!cookieSeguras);
+
+
+
+            if(cookieSeguras.nombre && cookieSeguras.valor ){
+                const intento= cookieSeguras.valor - resultado;
+
+                if(intento<=0){
+
+                    response
+                        .cookie('valor', 100, {signed:true})
+                        .status(200)
+                        .send(
+                            {NombreUsuario: `${cookieSeguras.nombre}`,
+                                Resultado: `${resultado}`,
+                                Mensaje: "Se terminaron sus puntos"})
+                }else{
+                    response
+                        .cookie('valor', intento, {signed:true})
+                        .status(200)
+                        .send({
+                            NombreUsuario:  `${cookieSeguras.nombre}`,
+                            Resultado:  `${resultado}`,
+                            ValorActual : `${intento}`})
+
+                }
+
+
             }
+            else{
 
-            if(!cookieSegura.userName){
-                response.cookie('nombre', 'Jenny', {signed:true});
-            }
-            if(!cookieSegura.valor){
-                response.cookie('valor', 100, {signed:true})
-            }
-
-            const intento= cookieSegura.valor - resultado;
-            if(intento<=0){
-                response.cookie('valor', 100, {signed:true})
-                response.status(200).send({NombreUsuario: `${cookieSeguras}`,
-                    Resultado: `${resultado}`,
-                    Mensaje: "Se terminaron sus puntos"})
-            }else{
-                response.cookie('valor', intento, {signed:true})
-                response.status(200).send({
-                    'NombreUsuario: ': `${cookieSeguras}`, 'Resultado: ': `${resultado}`})
-                //console.log(cookieSeguras);
+                if (!cookieSeguras.nombre){
+                    response.cookie( 'nombre', 'Jenny', {signed:true});
+                }
+                if (!cookieSeguras.valor) {
+                    response.cookie('valor', 100, {signed: true})
+                }
+                response.send('Creado');
 
             }
-
         }else {
             response.status(400).send({mensaje: 'Error, Parámetros inválidos', error: 400});
         }
@@ -247,7 +254,7 @@ export class AppController {
         }else {
             response.status(400).send({mensaje: 'Error, Parámetros inválidos', error: 400});
         }
-        
+
     }
 
 
