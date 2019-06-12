d93d5f2facb7e6dfd8ac51b103a52f41fb9e379a
JennyCastro <jennykarinacastro@gmail.com>
2019-06-12 16:01:47 -0500

    Conexión BD

--- 01-http/02-servidor-web-nodejs/api/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //el directorio que usa es el que se llama vista
     app.use(express.static ('publico'));
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
 
